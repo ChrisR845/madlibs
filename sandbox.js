56b6c393357745d6cefaea77a23c83f1fb12fb5a
Christopher <127crodriguezrufino@frhsd.com>
2025-12-18 12:51:06 -0500

    .

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,13 @@
 let btn = document.getElementById("btn");
+let placeHolders = document.getElementById("");
 btn.addEventListener("click", getWords);
 function getWords(){
   let words = document.querySelectorAll("input[type=text]");
-    [console.log = words, index]
 let placeHolders = document.querySelectorAll(".userInput");
 placeHolders.forEach((placeHolder, index) => {
+  placeHolder.innerHTML = words[index].value;
   console.log(placeHolder, index);
 })
+document.getElementById("story-screen").style.display="block"
+//show hidden story
 }
